20b175ba4a142fbbed86aeae75111f5ed6266817
Jakob <jakob.vogt@bo-bremen.de>
2017-08-07 20:10:08 +0200

    Event Hook; Some minor Issues

--- extension.js
+++ extension.js
@@ -1,38 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 
 function activate(context) {
 
-    console.log('Congratulations, your extension "reload-on-save" is now active!');
+    vscode.workspace.onDidSaveTextDocument(function () {
+        console.log('trigger reload');
+    });
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand(
-        'extension.sayHello',
+    var startServer = vscode.commands.registerCommand(
+        'extension.startServer',
         function () {
-            // The code you place here will be executed every time your command is executed
-
-            // Display a message box to the user
-            vscode.window.showInformationMessage('Hello World!');
+            console.log('start server');
         }
     );
 
-
-    var triggerReload = vscode.commands.registerCommand(
-        'extension.triggerReload',
+    var stopServer = vscode.commands.registerCommand(
+        'extension.stopServer',
         function () {
-            console.log('triggering');
+            console.log('stop server');
         }
     );
 
-    context.subscriptions.push(disposable);
-    context.subscriptions.push(triggerReload);
+    var restartServer = vscode.commands.registerCommand(
+        'extension.restartServer',
+        function () {
+            console.log('restart server');
+        }
+    );
+
+    context.subscriptions.push(startServer);
+    context.subscriptions.push(stopServer);
+    context.subscriptions.push(restartServer);
 }
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {
+    console.log('stop server');
 }
 exports.deactivate = deactivate;
